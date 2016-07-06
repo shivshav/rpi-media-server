c9c406099a908739a19100748f2896ae0df74ce4
shiv <sprasad0603@gmail.com>
2016-07-05 23:31:13 -0700

    Update createContainers script to use custom sickrage container with proper volume arguments

---
+++ createContainers.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+TORRENT_DL_DIR=${TORRENT_DL_DIR:-/mnt/media/download/torrent}
+TR_CONFIG_DIR=${TR_CONFIG_DIR:-/mnt/media/config/transmission-daemon}
+
+SR_TV_SHOW_DIR=${SR_TV_SHOW_DIR:-/mnt/media/tv_shows}
+SR_CONFIG_DIR=${SR_CONFIG_DIR:-/mnt/media/config/sickrage}
+SR_POST_PROC_DIR=${SR_POST_PROC_DIR:-$TORRENT_DL_DIR/completed/tv}
+
+# TODO: This doesn't create subdirs if toplevel exists 
+# TODO: Unfortunately we can't rely on the container to already have these dirs as the upstream image already declares
+# the mounted directory as a volume, thus making any mkdir's on that dir in the Dockerfile useless
+[[ -d $TORRENT_DL_DIR ]] || mkdir -p $TORRENT_DL_DIR/{completed,incomplete,torrents}
+
+[[ -d $TR_CONFIG_DIR ]] || mkdir -p $TR_CONFIG_DIR
+[[ -d $SR_TV_SHOW_DIR ]] || mkdir -p $SR_TV_SHOW_DIR
+[[ -d $SR_CONFIG_DIR ]] || mkdir -p $SR_CONFIG_DIR
+
+# HomeBrewed transmission container
+docker run -d \
+    -p 9091:9091 \
+    -p 51413:51413 \
+    -p  51413:51413/udp \
+    -v $TORRENT_DL_DIR:/var/lib/transmission-daemon/downloads \
+    -v $TR_CONFIG_DIR:/etc/transmission-daemon \
+    --restart='always' \
+    --name transmission \
+    shiv/rpi-transmission
+
+# Create HomeBrewed SickRage container
+docker run -d \
+    -p 8081:8081 \
+    -v $SR_TV_SHOW_DIR:/data/tvshows \
+    -v $SR_CONFIG_DIR:/opt/SickRage/data \
+    -v $SR_POST_PROC_DIR:/data/completed_downloads \
+    --restart=always \
+    --name sickrage \
+    shiv/rpi-sickrage
+
+# Create flexget container
