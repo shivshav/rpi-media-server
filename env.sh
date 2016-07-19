36685d6461c85e9eb9390a0f010bf0fb17590a31
shiv <sprasad0603@gmail.com>
2016-07-18 20:54:01 -0700

    Update env.sh to have some sensible defaults. Update rpi-sickarage referenece to point to fixed version which sets TRAKT_ACCOUNT correctly

--- env.sh
+++ env.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export TRAKT_ACCOUNT=shivshav
-export SKIP_TRAKT_AUTH=true
+export SKIP_TRAKT_AUTH=false
 export TORRENT_DL_DIR=/mnt/media/download/torrent
 export TR_CONFIG_DIR=/mnt/media/config/transmission-daemon
 export OVERWRITE_CONFIG=true
-export TR_USERNAME=shiv
+export TR_USERNAME=shivshav
 export TR_PASSWORD=glipglop
 export SR_TV_SHOW_DIR=/mnt/media/tv_shows
 export SR_CONFIG_DIR=/mnt/media/config/sickrage
