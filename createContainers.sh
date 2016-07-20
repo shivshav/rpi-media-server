f396088311eb13cf4328aa7c81c92269862e33e9
shiv <sprasad0603@gmail.com>
2016-07-20 00:17:44 -0700

    Add --option to createContainer.sh to ensure containers are rebuilt every time

--- createContainers.sh
+++ createContainers.sh
@@ -2,4 +2,4 @@
 source env.sh
 rm -rf docker-compose.yml
 perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' < compose-template.yml > docker-compose.yml
-docker-compose up
+docker-compose up --build
