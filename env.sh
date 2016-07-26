371191d1fdd0c15200be457090128c71b8240c3d
shiv <sprasad0603@gmail.com>
2016-07-25 22:40:43 -0700

    Update to use new flexget container which does renaming. Update environment variables and compose template volumes to support this change

--- env.sh
+++ env.sh
@@ -9,4 +9,6 @@ export TR_PASSWORD=glipglop
 export SR_TV_SHOW_DIR=/mnt/media/tv_shows
 export SR_CONFIG_DIR=/mnt/media/config/sickrage
 export SR_POST_PROC_DIR=$TORRENT_DL_DIR/completed/tv
+export FG_MOVIE_DIR=/mnt/media/movies
 export FG_CONFIG_DIR=/mnt/media/config/flexget
+export FG_POST_PROC_DIR=$TORRENT_DL_DIR/completed/movie
